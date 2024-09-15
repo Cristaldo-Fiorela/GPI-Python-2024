49f9d7cfddd07d78d9efdd797750bf82e3afb7f5
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2024-09-15 19:05:11 -0300

    parte 1 ejercicio 2

---
+++ Estructura de Datos/TP_1/ejercicio_2.py
@@ -0,0 +1,45 @@
+"""
+La universidad ahora pide un programa que permita cargar las notas de dos exámenes y obtener el promedio. Además deberá determinar si el alumno aprobó el último examen (nota mayor o igual a 7), en caso contrario informar que desaprobó.
+Además se desea saber si el alumno mejoró, empeoró o mantuvo su desempeño entre ambos parciales. Para ello se deberá informar "Mejoró su desempeño" si la nota del segundo parcial es mayor que la del primero, "Mantuvo la nota" si ambas notas son iguales o "Empeoró su desempeño" si la nota del primer parcial es mayor que la del segundo.
+Finalmente, la universidad desea saber si el alumno promocionó la materia (promedio mayor o igual a 7), debe rendir final (promedio mayor o igual a 4) o debe recursar.
+"""
+
+notas = [];
+
+def valido(nota):
+  if isinstance(nota, (int, float)):
+    if (nota < 0):
+      print('Ingrese un valor valido no negativo.');
+      return False;
+    return True;
+  elif isinstance(nota, str):
+    nota_lower = nota.lower();
+    if (nota_lower == 'ok'):
+      return 'ok'
+  else:
+    print('Ingrese un valor valido.');
+    return False
+
+while (True):
+  input_user = input("Ingrese las notas a cargar, cuando desea finalizar el proceso por favor escriba: 'ok' ");
+
+  if (input_user.lower() == 'ok'):
+    break;
+
+  valor = float(input_user);
+  if valido(valor):
+    notas.append(valor);
+
+def promedio(notas): 
+  total = 0;
+  cantidad = len(notas);
+
+  for nota in notas:
+    total += nota;
+
+  promedio = total / cantidad;
+  return promedio;
+
+promedio_total = promedio(notas);
+
+print(f"El promedio es: {promedio_total}")
