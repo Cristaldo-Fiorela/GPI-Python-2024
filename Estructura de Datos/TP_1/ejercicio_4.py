6fdbc9d4fd6bd161b815c9c90e94c2a6b909c592
Cristaldo-Fiorela <cristaldofiorela@gmail.com>
2024-10-05 17:26:14 -0300

    ejercicio 4 - parte A

--- Estructura de Datos/TP_1/ejercicio_4.py
+++ Estructura de Datos/TP_1/ejercicio_4.py
@@ -17,7 +17,12 @@ La universidad está mejorando sus sistemas y ha solicitado lo siguiente:
     Finalmente se pide calcular el costo del comedor. La cuota vale $ 1250 por día y se desea imprimir un informe que muestre la cantidad de días (de 1 a 6) y el costo total (for). Por ejemplo: 1 día cuesta $ 1250, 2 días cuestan $ 2500
 '''
 
-# A - Listado de aulas
+# VARIABLES A UTILIZAR
+dias = [1, 2, 3, 4, 5, 6]
+aula_par = 'A-300';
+aula_impar = 'A-315';
+
+# UTILS
 def es_par(num):
   """
     Verifica si un día dado es par.
@@ -27,10 +32,22 @@ def es_par(num):
 
     Returns:
       bool: 
-        - True si el número del día es par.
-        - False si el número del día es impar.
+        - True si el número es par.
+        - False si el número es impar.
   """
   if (num % 2 == 0):
     return True;
   else:
     return False;
+
+# A - Listado de aulas
+
+print('======Listado de aulas=======');
+print(f"{'Día':<5} {'Aula':<5}")
+
+for dia in dias:
+  if(es_par(dia)):
+    print(f"{dia:<5} {aula_par:<5}");
+  else:
+    print(f"{dia:<5} {aula_impar:<5}");
+
